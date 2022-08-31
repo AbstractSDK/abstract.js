f5a8b8e0054fb939d09c04c47cf15071bc0c45b8
adairrr <32375605+adairrr@users.noreply.github.com>
2022-08-31 21:27:00 +0200

    Format and build

--- dist/modules/Staking.types.d.ts
+++ dist/modules/Staking.types.d.ts
@@ -13,55 +13,42 @@ export declare type RequestMsg = {
     delegate: {
         amount: Uint128;
         validator: string;
-        [k: string]: unknown;
     };
 } | {
     undelegate_from: {
         amount?: Uint128 | null;
         validator: string;
-        [k: string]: unknown;
     };
 } | {
-    undelegate_all: {
-        [k: string]: unknown;
-    };
+    undelegate_all: {};
 } | {
     redelegate: {
         amount?: Uint128 | null;
         destination_validator: string;
         source_validator: string;
-        [k: string]: unknown;
     };
 } | {
     set_withdraw_address: {
         new_withdraw_address: string;
-        [k: string]: unknown;
     };
 } | {
     withdraw_delegator_reward: {
         validator: string;
-        [k: string]: unknown;
     };
 } | {
-    withdraw_all_rewards: {
-        [k: string]: unknown;
-    };
+    withdraw_all_rewards: {};
 };
 export declare type Uint128 = string;
 export declare type ApiExecuteMsg = {
     update_traders: {
         to_add?: string[] | null;
         to_remove?: string[] | null;
-        [k: string]: unknown;
     };
 } | {
-    remove: {
-        [k: string]: unknown;
-    };
+    remove: {};
 };
 export interface ApiRequestMsgForRequestMsg {
     proxy_address?: string | null;
     request: RequestMsg;
-    [k: string]: unknown;
 }
 export declare type QueryMsg = string;
