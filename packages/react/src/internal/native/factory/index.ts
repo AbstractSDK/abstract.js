9a65e54dc5a7cc9696ebd6ff28d55123b34b3044
hotwater <vlad.dalechin@gmail.com>
2023-12-26 18:34:56 +0200

    refactor: drop internal methods from `react` package
    Those are not longer needed as if the SDK consumer
    would like to use those, he could generate such
    with `@abstract-money/cli`.

--- packages/react/src/internal/native/factory/index.ts
+++
@@ -1,5 +0,0 @@
-export { useConfig } from './use-config'
-export { useCreateAccount } from './use-create-account'
-export { useOwnership } from './use-ownership'
-export { useUpdateConfig } from './use-update-config'
-export { useUpdateOwnership } from './use-update-ownership'
--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'tsup'
 
-import path from 'path'
-import fs from 'fs/promises'
 import { getConfig } from '../../scripts/tsup'
 import { dependencies, peerDependencies } from './package.json'
 
@@ -9,20 +7,7 @@ export default defineConfig(
   getConfig({
     //dev: process.env.DEV === 'true',
     experimentalDts: false,
-    entry: [
-      'src/index.ts',
-      'src/utils/index.ts',
-      'src/internal/index.ts',
-      'src/internal/account/index.ts',
-      'src/internal/apps/challenge/index.ts',
-      'src/internal/cw-plus/cw-20/index.ts',
-      'src/internal/cw-plus/cw-20-ics/index.ts',
-      'src/internal/cw-plus/cw-3-flex-multisig/index.ts',
-      'src/internal/cw-plus/cw-4-group/index.ts',
-      'src/internal/native/ans/index.ts',
-      'src/internal/native/factory/index.ts',
-      'src/internal/native/registry/index.ts',
-    ],
+    entry: ['src/index.ts', 'src/utils/index.ts'],
     external: [...Object.keys(dependencies), ...Object.keys(peerDependencies)],
     platform: 'browser',
     banner: {
