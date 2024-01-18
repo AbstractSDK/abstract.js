37174725a5455e095c0dedaf31b31aa20c7ad888
hotwater <vlad.dalechin@gmail.com>
2024-01-18 16:41:30 +0200

    fix: resolve review comment
    https://github.com/AbstractSDK/abstract.js/pull/30#discussion_r1452885338

--- packages/core/src/actions/public/get-ans-host-address-from-version-control.ts
+++ packages/core/src/actions/public/get-ans-host-address-from-version-control.ts
@@ -1,41 +1,18 @@
 import { CosmWasmClient } from '@cosmjs/cosmwasm-stargate'
-
 import {
-  VersionControlQueryClient,
-  VersionControlTypes,
-} from '../../codegen/abstract'
-import { versionControlModuleToAddress } from '../../utils/version-control/version-control-module-to-address'
-const ANS_HOST_MODULE_NAME = 'ans-host'
+  CommonModuleNames,
+  getModuleAddressFromVersionControl,
+} from './get-module-address-from-version-control'
 
 export async function getAnsHostAddressFromVersionControl(
   cosmWasmClient: CosmWasmClient,
   versionControlAddress: string,
   version?: string,
 ) {
-  const versionControlClient = new VersionControlQueryClient(
+  return getModuleAddressFromVersionControl(
+    CommonModuleNames.ANS_HOST_MODULE_NAME,
     cosmWasmClient,
     versionControlAddress,
+    version,
   )
-
-  const [ansHostAddress] = await versionControlClient
-    .modules({
-      infos: [
-        {
-          name: ANS_HOST_MODULE_NAME,
-          namespace: 'abstract',
-          version: version ? { version } : 'latest',
-        } satisfies VersionControlTypes.ModuleInfo,
-      ],
-    })
-    .then(({ modules }) =>
-      modules.map(({ module }) => versionControlModuleToAddress(module)),
-    )
-
-  if (!ansHostAddress) {
-    throw new Error(
-      `Could not get ansHostAddress for version ${version} from registry ${versionControlAddress}`,
-    )
-  }
-
-  return ansHostAddress
 }
