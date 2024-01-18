a7245d57bd96c491feaddb767a4255fe4dfc2022
hotwater <vlad.dalechin@gmail.com>
2024-01-18 16:34:51 +0200

    nit: drop comment

--- packages/core/src/utils/account-id/account-id-to-parameter.ts
+++ packages/core/src/utils/account-id/account-id-to-parameter.ts
@@ -1,6 +1,5 @@
 import { VersionControlTypes } from 'src/codegen/abstract'
 
-// TODO: Figure out where is this used
 export function accountIdToParameter(id: VersionControlTypes.AccountId) {
   return { seq: id.seq, trace: id.trace }
 }
