694e3690e18f0a03b4516b56e262ad5103f6d85a
hotwater <vlad.dalechin@gmail.com>
2024-01-18 16:22:41 +0200

    fix: resolve review comment
    https://github.com/AbstractSDK/abstract.js/pull/30#discussion_r1452870159

--- packages/core/src/actions/get-ans-token-from-api.ts
+++ packages/core/src/actions/get-ans-token-from-api.ts
@@ -1,34 +1,14 @@
-import { request } from 'graphql-request'
-import { gql } from '../codegen/gql'
-import { ansTokenFromApi } from '../utils/tokens/ans/ans-token-from-api'
 import { AnsId } from '../utils/tokens/ans/types'
+import { getAnsTokensFromApi } from './get-ans-tokens-from-api'
 
 export async function getAnsTokenFromApi(
   apiUrl: string,
   chainId: string,
   id: AnsId,
 ) {
-  const result = await request(
-    apiUrl,
-    gql(/* GraphQL */ `
-      query AssetQuery($chain: ID!, $filter: IdsFilter!) {
-        ans(chain: $chain) {
-          assets(filter: $filter) {
-            id
-            type
-            address
-          }
-        }
-      }
-  `),
-    {
-      chain: chainId,
-      filter: { ids: [id] },
-    },
+  const result = await getAnsTokensFromApi(apiUrl, chainId, [id]).then(
+    (tokens) => tokens[0],
   )
-
-  const token = result.ans.assets[0]
-  if (!token) throw new Error(`Token with id ${id} not found`)
-
-  return ansTokenFromApi(token)
+  if (!result) throw new Error(`Cannot find token with id ${id}`)
+  return result
 }
