7eb8402365873221580bad5c10f42069c09086c0
hotwater <vlad.dalechin@gmail.com>
2024-05-21 23:44:43 +0300

    nit: forgot export

--- packages/react/src/hooks/public/index.ts
+++ packages/react/src/hooks/public/index.ts
@@ -1,5 +1,6 @@
 export * from './use-cosm-wasm-client'
 export * from './use-abstract-module-version'
+export * from './use-accounts-base-addresses-from-api'
 export * from './use-account-factory-query-client-from-api'
 export * from './use-account-factory-config-from-api'
 export * from './use-remote-hosts'
